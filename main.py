fd55d9b6d71a5fab0d6178f7ccf252df1a17fb23
ShauryaAg <agarwal.lucky.shaurya11@gmail.com>
2020-07-23 19:22:56 +0530

    Changes in class names

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='C:\\Users\\Your_Name\\Downloads\\chromedriver_win32\\chromedriver.exe') #Change the executable_path to the correct path of your chromedriver.exe
+# Change the executable_path to the correct path of your chromedriver.exe
+driver = webdriver.Chrome(
+    executable_path='D:\Documents\CodingBlocks\CNinjas\Selenium\chromedriver_win32\chromedriver.exe')
 
-driver.get('https://web.whatsapp.com/')
-
-
-name = input("Enter the name of user: ")
-msg = input("Enter message to be sent: ")
-count = int(input("Enter count: "))
-
-input()
-
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
-
-msg_box = driver.find_element_by_class_name('_1Plpp')
+executor_url = driver.command_executor._url
+session_id = driver.session_id
 
 
-for i in range(count):
-    msg_box.send_keys(msg)
-    send_button = driver.find_element_by_xpath('//button[@class="_35EW6"]')
-    send_button.click()
+print(executor_url)
+print(session_id)
+
+# driver.get('https://web.whatsapp.com/')
+
+
+# name = input("Enter the name of user: ")
+# msg = input("Enter message to be sent: ")
+# count = int(input("Enter count: "))
+
+# input()
+
+# user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+# user.click()
+
+# msg_box = driver.find_element_by_xpath(
+#     '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+
+# for i in range(count):
+#     msg_box.send_keys(msg)
+#     send_button = driver.find_element_by_xpath('//button[@class="_1U1xa"]')
+#     send_button.click()
